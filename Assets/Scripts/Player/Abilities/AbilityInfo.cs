b4fad789a3fe123333e811f9608c0a0131a2903f
Alicia Shanahan <alicia02s@berkeley.edu>
2021-09-10 21:21:17 -0700

    project 1 part 1 submission

---
+++ Assets/Scripts/Player/Abilities/AbilityInfo.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class AbilityInfo
+{
+    #region Editor Variables
+    [SerializeField]
+    [Tooltip("How much power this ablility has")]
+    private int m_Power;
+    public int Power {
+    	get {
+    		return m_Power;
+    	}
+    }
+
+    [SerializeField]
+    [Tooltip("If this is an attack that shoots something out, this value describes how far this attack can shoot")]
+    private int m_Range;
+    public int Range {
+    	get {
+    		return m_Range;
+    	}
+    }
+
+    
+    #endregion
+}
